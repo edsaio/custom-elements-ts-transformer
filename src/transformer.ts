5fe2d972d8e6c34f4d14ced0cdc06900d0fdf038
aelbore <aelbore.sg@gmail.com>
2018-11-14 13:37:17 +0800

    refactor visitor

--- src/transformer.ts
+++ src/transformer.ts
@@ -1,6 +1,7 @@
 import * as ts from 'typescript';
 
-import { classDeclaration } from './visitor'
+import { updateClass } from './update-class';
+import { createDefineStatement } from './create-define-statement';
 
 export function transformer(): Array<ts.TransformerFactory<ts.SourceFile>> {
   const tsSourceTransformers: Array<ts.TransformerFactory<ts.SourceFile>> = [
@@ -9,12 +10,38 @@ export function transformer(): Array<ts.TransformerFactory<ts.SourceFile>> {
   return tsSourceTransformers;
 }
 
+const customElementDecorator = (node: ts.Node) => {
+  return node.decorators.find((decorator) => {
+    const decoratorExpr = decorator.expression;
+    return ts.isCallExpression(decoratorExpr) && decoratorExpr.expression.getText() === 'customElement';
+  });
+}
+
 const transform = (context: ts.TransformationContext, sf: ts.SourceFile) => {
   const visitor: ts.Visitor = (node) => {
-    const value = classDeclaration(node);
-    if (value) {
-      return value 
+    if (ts.isClassDeclaration(node) && node.decorators && node.name) {    
+      const decorator = customElementDecorator(node);
+
+      if (decorator) {
+        const filteredDecorators = node.decorators.filter((d) => d !== decorator);
+        node.decorators = (filteredDecorators.length > 0) 
+          ? ts.createNodeArray(filteredDecorators)
+          : undefined;
+  
+        const argument = (<ts.CallExpression>decorator.expression).arguments[0] as ts.ObjectLiteralExpression;
+        return [
+          updateClass(node, argument), 
+          createDefineStatement(node, argument)
+        ]
+      }
+      
+      return node;
     }
+
+    if (ts.isDecorator(node)) {
+      return undefined;
+    }
+
     return ts.visitEachChild(node, (child) => visitor(child), context);
   }
   return ts.visitNode(sf, visitor);
